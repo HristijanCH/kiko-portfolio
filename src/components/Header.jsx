bdabc0f617f7f4f9a076d81b1063d97869b629e5
SmartW0rker <hristijanchochevski@gmail.com>
2025-01-26 16:54:36 +0100

    adding header and navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import Navbar from "./Navbar.jsx";
+
+const Header = () => {
+    return (
+        <header className=
+                    "fixed top-0 left-0 w-full h-20 flex items-center
+                     z-40 bg-gradient-to-b from-zinc-900/0">
+            <div className="max-w-screen-2xl w-full mx-auto px-4
+            flex justify-between items-center md:px-6 md:grid-cols-[1fr,3fr,1fr]"
+                 style={{border: "2px solid red"}}>
+                <h1>
+                    <a
+                        href="/"
+                        className="logo"
+                    >
+                        <img
+                            src="images/logo.svg"
+                            width={40}
+                            height={40}
+                            alt="Hristijan Chochevski"
+                        />
+                    </a>
+                </h1>
+                <div className="relative md:justify-self-center">
+                    <button
+                        className="menu-btn"
+                        onClick={null}
+                    >
+                        <span className="material-symbols-rounded">
+                            menu
+                        </span>
+                    </button>
+                    <Navbar/>
+                </div>
+                <a href="#contact" className="">
+                    Contact Me
+                </a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
